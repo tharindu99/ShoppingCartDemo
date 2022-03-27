efec258784c4c3fe5b8de971d5951a56b807a519
legator-admin <hello@legatorsolutions.com>
2022-03-27 19:29:41 +0530

    project init

---
+++ cart-service/src/main/java/com/abc/cartservice/controller/CartController.java
@@ -0,0 +1,34 @@
+package com.abc.cartservice.controller;
+
+
+import com.abc.cartservice.entity.Cart;
+import com.abc.cartservice.service.CartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cart")
+@Slf4j
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @GetMapping("/")
+    public List<Cart> getAllItems(){
+        return cartService.getAllItems();
+    }
+
+    @PostMapping("/add/")
+    public void addItemToCart(@RequestBody Cart productSelection){
+        log.info("POST: add item to cart");
+        cartService.addItemToCart(productSelection);
+    }
+
+
+
+}
